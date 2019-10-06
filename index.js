b261fb60a664268e33611eea9ca88fd741cc844e
Stuart Grant <grantsru@gmail.com>
2019-10-06 09:02:04 -0400

    Conflicts

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 "use strict";
-<<<<<<< HEAD
-function greeter(person) {
-    console.log("Hello " + person + "!");
-}
-var user = "Stuart";
-greeter(user);
-=======
 var Program = /** @class */ (function () {
     function Program() {
     }
@@ -14,4 +7,3 @@ var Program = /** @class */ (function () {
     };
     return Program;
 }());
->>>>>>> 7314bbb92f8e053581d4668ce6bc33d8be41eb5f
--- index.ts
+++ index.ts
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-function greeter(person: string): void {
-  console.log(`Hello ${person}!`);
-}
-
-const user: string = "Stuart";
-
-greeter(user);
-=======
 class Program {
   public static Main(): void {
     console.log("Hello World!");
   }
 }
->>>>>>> 7314bbb92f8e053581d4668ce6bc33d8be41eb5f
